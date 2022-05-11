03cbf0cc8d048fabdd31cb43a7a040950eae515a
TheNoice <61051924+TheNoice@users.noreply.github.com>
2022-05-11 17:29:05 +0300

    class2
    1341

---
+++ DocumentsSenderConsole/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsSenderConsole
+{
+    class Class2
+    {
+        public int MyProperty2 { get; set; }
+    }
+}
